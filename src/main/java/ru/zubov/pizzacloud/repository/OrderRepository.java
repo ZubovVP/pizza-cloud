16c8dab383ac1bf8356b8e4e787cd0b75360245e
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-04-27 23:50:04 +0300

    Refactoring

--- src/main/java/ru/zubov/pizzacloud/repository/OrderRepository.java
+++ src/main/java/ru/zubov/pizzacloud/repository/OrderRepository.java
@@ -1,8 +1,18 @@
 package ru.zubov.pizzacloud.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.zubov.pizzacloud.entity.PizzaOrder;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<PizzaOrder, Long> {
-    PizzaOrder save(PizzaOrder order);
+
+    List<PizzaOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, LocalDateTime startDate, LocalDateTime endDate);
+
+    List<PizzaOrder> findByDeliveryNameAndDeliveryCity(String deliveryName, String deliveryCity);
+
+    @Query("SELECT o FROM PizzaOrder o where o.deliveryCity='Seattle'")
+    List<PizzaOrder> readOrdersDeliveredInSeattle();
 }
