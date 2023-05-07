939ff6f10ca805504fd67b175b513683323c0395
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-05-07 23:53:47 +0300

    Create table for users

--- src/main/java/ru/zubov/pizzacloud/entity/User.java
+++ src/main/java/ru/zubov/pizzacloud/entity/User.java
@@ -1,9 +1,6 @@
 package ru.zubov.pizzacloud.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,18 +14,20 @@ import java.util.List;
 
 @Entity
 @Data
+@Table(name = "Users")
 @NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
 @RequiredArgsConstructor
 public class User implements UserDetails {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    private Long id;
+    private long id;
     private final String username;
     private final String password;
     private final String fullname;
     private final String street;
     private final String city;
+    @Column(name = "st")
     private final String state;
     private final String zip;
     private final String phoneNumber;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,12 +27,25 @@ create table if not exists Pizza (
 
 create table if not exists Pizza_Order (
     id identity primary key,
+    delivery_name varchar(50) not null,
+    delivery_street varchar(50) not null,
+    delivery_city varchar(50) not null,
+    delivery_state varchar(2) not null,
+    delivery_zip varchar(10) not null,
+    cc_number varchar(16) not null,
+    cc_expiration varchar(5) not null,
+    cc_cvv varchar(3) not null,
+    placed_at timestamp not null
+    );
+
+create table if not exists Users (
+    id identity primary key,
     username varchar(50) not null,
     password varchar(50) not null,
     fullname varchar(50) not null,
     street varchar(50),
     city varchar(50),
-    state varchar(50),
+    st varchar(50),
     zip varchar(50),
     phoneNumber varchar(50) not null
     );
