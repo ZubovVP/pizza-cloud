4f0fa31f699c26d3cbf17efc04fafc2da3d85942
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-05-08 23:54:13 +0300

    Try to correct mistake for create order

--- src/main/java/ru/zubov/pizzacloud/PizzaCloudApplication.java
+++ src/main/java/ru/zubov/pizzacloud/PizzaCloudApplication.java
@@ -2,6 +2,17 @@ package ru.zubov.pizzacloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class PizzaCloudApplication {
@@ -10,17 +21,20 @@ public class PizzaCloudApplication {
         SpringApplication.run(PizzaCloudApplication.class, args);
     }
 
-//    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript("schema.sql")
-//                .addScript("data-h2.sql")
-//                .build();
-//    }
-//
-//    @Bean
-//    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-//        return new JdbcTemplate(dataSource);
-//    }
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> usersList = new ArrayList<>();
+        usersList.add(new User(
+                "buzz", encoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        usersList.add(new User(
+                "woody", encoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(usersList);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
+++ src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
@@ -1,49 +1,37 @@
 package ru.zubov.pizzacloud.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorityAuthorizationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import ru.zubov.pizzacloud.entity.User;
-import ru.zubov.pizzacloud.repository.UserRepository;
-import ru.zubov.pizzacloud.service.UserDetailsService;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepo) {
-        return username -> {
-            User user = userRepo.findByUsername(username);
-            if (user != null){
-                return user;
-            }
-            throw new UsernameNotFoundException("User " + username + " not found");
-        };
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests()
-                .requestMatchers("/design", "/orders").access(AuthorityAuthorizationManager.hasRole("USER"))
-                .requestMatchers("/", "/**").permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/design", true)
-//                .loginProcessingUrl("/authenticate")
-//                .usernameParameter("user")
-//                .passwordParameter("pwd")
-                .and()
-                .build();
-    }
-}
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(UserRepository userRepo) {
+//        return username -> {
+//            User user = userRepo.findByUsername(username);
+//            if (user != null){
+//                return user;
+//            }
+//            throw new UsernameNotFoundException("User " + username + " not found");
+//        };
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http.authorizeHttpRequests()
+//                .requestMatchers("/design", "/orders").access(AuthorityAuthorizationManager.hasRole("USER"))
+//                .requestMatchers("/", "/**").permitAll()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/design", true)
+////                .loginProcessingUrl("/authenticate")
+////                .usernameParameter("user")
+////                .passwordParameter("pwd")
+//                .and()
+//                .build();
+//    }
+//}
