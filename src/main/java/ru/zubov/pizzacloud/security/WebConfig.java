f03a1ba2780ac477829e1fce96df929ff1a54634
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-05-10 23:56:31 +0300

    Try to find mistake

--- src/main/java/ru/zubov/pizzacloud/security/WebConfig.java
+++ src/main/java/ru/zubov/pizzacloud/security/WebConfig.java
@@ -1,11 +1,17 @@
 package ru.zubov.pizzacloud.security;
 
-//@Configuration
-//public class WebConfig  implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-////        registry.addViewController("/").setViewName("home");
-////        registry.addViewController("/login");
-//    }
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/design").setViewName("home");
+        registry.addViewController("/orders").setViewName("home");
+        registry.addViewController("/login");
+    }
+}
