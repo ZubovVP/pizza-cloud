e0b3e4d391da9bd7756edef053e86dfcaa3ab2c3
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-05-12 23:56:58 +0300

    Refactoring SecurityConfig

--- src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
+++ src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
@@ -5,28 +5,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.zubov.pizzacloud.entity.User;
+import ru.zubov.pizzacloud.repository.UserRepository;
+import ru.zubov.pizzacloud.service.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService(UserRepository userRepo) {
-//        return username -> {
-//            User user = userRepo.findByUsername(username);
-//            if (user != null){
-//                return user;
-//            }
-//            throw new UsernameNotFoundException("User " + username + " not found");
-//        };
-//    }
-//
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null){
+                return user;
+            }
+            throw new UsernameNotFoundException("User " + username + " not found");
+        };
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
