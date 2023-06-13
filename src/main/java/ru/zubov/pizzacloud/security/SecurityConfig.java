9687b40279b6fb4297a7c3111973b775ac26fd5f
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-06-13 23:33:08 +0300

    Solved problem

--- src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
+++ src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.zubov.pizzacloud.entity.User;
 import ru.zubov.pizzacloud.repository.UserRepository;
 
 @Configuration
@@ -24,27 +24,15 @@ public class SecurityConfig{
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
-        return manager;
-
-//        return username -> {
-//            User user = userRepository.findByUsername(username);
-//            if (user != null) return user;
-//            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
-//        };
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+        };
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-//        http.csrf()
-//                .securityMatcher("/api/**")
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().hasRole("ADMIN")
-//                )
-//                .httpBasic(withDefaults());
-//        return http.build();
 
         return http.csrf().disable().authorizeHttpRequests()
                 .requestMatchers("/design", "/orders").access(AuthorityAuthorizationManager.hasRole("USER"))
