9df7bea2a1811a4a6099e066b8baafb8913826af
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-07-10 23:36:03 +0300

    Create patchMapping

--- src/main/java/ru/zubov/pizzacloud/controller/PizzaController.java
+++ src/main/java/ru/zubov/pizzacloud/controller/PizzaController.java
@@ -51,4 +51,35 @@ public class PizzaController {
         order.setId(orderId);
         return orderRepository.save(order);
     }
+
+    @PatchMapping(path="/{orderId}", consumes="application/json")
+    public PizzaOrder patchOrder(@PathVariable("orderId") Long orderId,
+                                @RequestBody PizzaOrder patch) {
+        PizzaOrder order = orderRepository.findById(orderId).get();
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepository.save(order);
+    }
 }
