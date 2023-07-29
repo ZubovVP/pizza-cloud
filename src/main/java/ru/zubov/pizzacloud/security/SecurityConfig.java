2fb5986e69c08a3b9a1b08cfdfd2635009406496
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-07-29 23:45:23 +0300

    Add security configuration

--- src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
+++ src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -49,7 +50,7 @@ public class SecurityConfig{
                 .defaultSuccessUrl("/design", true)
                 .loginPage("/login").permitAll()
                 .and()
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .build();
     }
 }
