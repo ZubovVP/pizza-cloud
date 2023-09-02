d0b5e2599518fae8716b49c8410756121add86b7
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-09-02 23:29:10 +0300

    Add test for TDD

--- src/test/java/ru/zubov/pizzacloud/controller/OrderControllerTest.java
+++ src/test/java/ru/zubov/pizzacloud/controller/OrderControllerTest.java
@@ -7,15 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.zubov.pizzacloud.config.OrderProps;
-import ru.zubov.pizzacloud.entity.User;
+import ru.zubov.pizzacloud.entity.PizzaOrder;
 import ru.zubov.pizzacloud.repository.OrderRepository;
 import ru.zubov.pizzacloud.repository.PizzaRepository;
 
-import static org.hamcrest.Matchers.any;
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -42,13 +44,19 @@ class OrderControllerTest {
     @Test
     public void testHomePage() throws Exception {
         Mockito.when(this.orderProps.getPageSize()).thenReturn(20);
-        Mockito.when(this.orderRepository.findByUserOrderByPlacedAtDesc(any(User.class), any(Pageable.class))).thenReturn(20);
+
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+
+        PizzaOrder pizzaOrder = new PizzaOrder();
+
+        Mockito.when(this.orderRepository.findByUserOrderByPlacedAtDesc(null, pageable)).thenReturn(List.of(pizzaOrder));
+
 
         mockMvc.perform(get("/orders"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("orderList"))
                 .andExpect(content().string(
-                        containsString("Order your pizza creations!")));
+                        containsString("Your orders")));
     }
 
 
