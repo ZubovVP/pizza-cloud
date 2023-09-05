24ad5757caeb4e78362600d522a8ce093d96e48d
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-09-05 21:13:06 +0300

    Solved mistake

--- src/main/java/ru/zubov/pizzacloud/IngredientByIdConverter.java
+++ src/main/java/ru/zubov/pizzacloud/IngredientByIdConverter.java
@@ -1,17 +1,15 @@
 package ru.zubov.pizzacloud;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import ru.zubov.pizzacloud.entity.Ingredient;
 import ru.zubov.pizzacloud.repository.IngredientRepository;
 
 @Component
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
-    private IngredientRepository ingredientRepository;
+    private final IngredientRepository ingredientRepository;
 
     @Override
     public Ingredient convert(String id) {
