30cf5d30c5f07c377855d6dde4fa2bff89e24af5
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-09-15 23:30:01 +0300

    Add test

---
+++ src/test/java/ru/zubov/pizzacloud/controller/DesignPizzaControllerTest.java
@@ -0,0 +1,41 @@
+package ru.zubov.pizzacloud.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.zubov.pizzacloud.repository.IngredientRepository;
+import ru.zubov.pizzacloud.repository.PizzaRepository;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(DesignPizzaController.class)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
+class DesignPizzaControllerTest {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private PizzaRepository pizzaRepository;
+
+
+    @Test
+    public void showDesignForm() throws Exception {
+        mockMvc.perform(get("/design"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("design"))
+                .andExpect(content().string(
+                        containsString("Design your pizza!")));
+    }
+}
