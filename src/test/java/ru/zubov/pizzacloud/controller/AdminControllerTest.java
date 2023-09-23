ff5c84fba8ee99e87588e9b821fc4b8a4c255fa4
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-09-23 22:28:04 +0300

    Test AdminController

---
+++ src/test/java/ru/zubov/pizzacloud/controller/AdminControllerTest.java
@@ -0,0 +1,60 @@
+package ru.zubov.pizzacloud.controller;
+
+import jakarta.servlet.ServletContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import ru.zubov.pizzacloud.IngredientByIdConverter;
+import ru.zubov.pizzacloud.service.OrderAdminService;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(AdminController.class)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("development")
+class AdminControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockBean
+    private IngredientByIdConverter ingredientByIdConverter;
+
+    @MockBean
+    private OrderAdminService adminService;
+
+
+    @Test
+    public void checkThenContextHaveDesignPizzaController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(webApplicationContext.getBean("adminController"));
+    }
+
+    @Test
+    void testDeleteAllOrders() throws Exception {
+        mockMvc.perform(post("/admin/deleteOrders"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/admin"));
+        verify(adminService, times(1)).deleteAllOrders();
+
+    }
+}
