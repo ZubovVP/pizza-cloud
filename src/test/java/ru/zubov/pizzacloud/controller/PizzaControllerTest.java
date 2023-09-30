a16bfcc8feae2ed1b482b70290e25df6dc7f4269
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-09-30 22:08:16 +0300

    Try test rest

--- src/test/java/ru/zubov/pizzacloud/controller/PizzaControllerTest.java
+++ src/test/java/ru/zubov/pizzacloud/controller/PizzaControllerTest.java
@@ -6,20 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.zubov.pizzacloud.IngredientByIdConverter;
 import ru.zubov.pizzacloud.repository.OrderRepository;
 import ru.zubov.pizzacloud.repository.PizzaRepository;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(PizzaController.class)
-@WithMockUser(username="admin",roles="ADMIN")
+@WithMockUser(username="ADMIN",roles="ADMIN")
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc(addFilters = false)
 class PizzaControllerTest {
@@ -41,25 +40,9 @@ class PizzaControllerTest {
 
     @Test
     void testDelete() throws Exception {
-//        mockMvc.perform(get("/api/pizza/1")
-//                .header("Access-Control-Request-Method", "GET")
-//                .header("Origin", "http://pizzacloud.com"))
-//                .andExpect(status().isOk());
-
-//        mockMvc.perform(get("/api/pizza/1").with(csfr()))
-//                .andExpect(status().isOk());
-
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-                "/api/pizza/1").accept(
-                MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-        System.out.println(result);
-
-
-//        mockMvc.perform(delete("/api/pizza/{orderId}", "1"))
-//                .andExpect(status().isNoContent())
-//                .andDo(print());
+        mockMvc.perform(get("/api/pizza/1")
+                .header("Access-Control-Request-Method", "GET")
+                .header("Origin", "http://pizzacloud.com"))
+                .andExpect(status().isOk());
     }
 }
