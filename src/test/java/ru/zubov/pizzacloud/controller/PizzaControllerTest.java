715a67fc0f6b435e2039a2258ad5016c050e92d2
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-10-07 23:23:28 +0300

    Add test patchOrder

--- src/test/java/ru/zubov/pizzacloud/controller/PizzaControllerTest.java
+++ src/test/java/ru/zubov/pizzacloud/controller/PizzaControllerTest.java
@@ -14,8 +14,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import ru.zubov.pizzacloud.IngredientByIdConverter;
 import ru.zubov.pizzacloud.entity.Pizza;
+import ru.zubov.pizzacloud.entity.PizzaOrder;
 import ru.zubov.pizzacloud.repository.OrderRepository;
 import ru.zubov.pizzacloud.repository.PizzaRepository;
 
@@ -26,9 +30,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -117,5 +119,61 @@ class PizzaControllerTest {
                 .andExpect(status().isOk());
 
         verify(orderRepository, times(1)).save(argThat(order -> order.getId() == 123L));
+    }
+
+    @PatchMapping(path = "/{orderId}", consumes = "application/json")
+    public PizzaOrder patchOrder(@PathVariable("orderId") Long orderId,
+                                 @RequestBody PizzaOrder patch) {
+        PizzaOrder order = orderRepository.findById(orderId).get();
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepository.save(order);
+    }
+
+    @Test
+    void testPatchOrder() throws Exception {
+        JSONObject pizzaOrder = new JSONObject();
+        pizzaOrder.put("id", "111");
+        pizzaOrder.put("deliveryName", "name");
+        pizzaOrder.put("deliveryStreet", "street");
+        pizzaOrder.put("deliveryCity", "city");
+        pizzaOrder.put("deliveryState", "state");
+        pizzaOrder.put("deliveryZip", "zip");
+        pizzaOrder.put("ccNumber", "123456789");
+        pizzaOrder.put("ccExpiration", "11/29");
+        pizzaOrder.put("ccCVV", "789");
+        pizzaOrder.put("ccCVV", "789");
+
+        when(orderRepository.findById(123L)).thenReturn(Optional.of(new PizzaOrder()));
+
+        mockMvc.perform(patch("/api/pizza/123")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(pizzaOrder.toString())
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk());
+
+        verify(orderRepository, times(1)).save(any());
     }
 }
