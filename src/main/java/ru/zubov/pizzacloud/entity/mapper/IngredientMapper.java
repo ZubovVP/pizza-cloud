8aef2cf78dbd0c77f31eeeef4ba6630baef0ab33
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-10-23 23:37:36 +0300

    Add Ingredient mapper

---
+++ src/main/java/ru/zubov/pizzacloud/entity/mapper/IngredientMapper.java
@@ -0,0 +1,15 @@
+package ru.zubov.pizzacloud.entity.mapper;
+
+import org.mapstruct.*;
+import ru.zubov.pizzacloud.entity.Ingredient;
+import ru.zubov.pizzacloud.entity.dtos.IngredientDto;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "cdi")
+public interface IngredientMapper {
+    Ingredient ingredientDtoToIngredient(IngredientDto ingredientDto);
+
+    IngredientDto ingredientToIngredientDto(Ingredient ingredient);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Ingredient updateIngredientFromIngredientDto(IngredientDto ingredientDto, @MappingTarget Ingredient ingredient);
+}
