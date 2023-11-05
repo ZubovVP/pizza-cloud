1ce52440b8887291c2a8c7703d5f22d8be6cd30f
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-11-05 23:53:30 +0300

    Cor security config

--- src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
+++ src/main/java/ru/zubov/pizzacloud/security/SecurityConfig.java
@@ -3,34 +3,20 @@ package ru.zubov.pizzacloud.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorityAuthorizationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true,
-        jsr250Enabled = true)
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(UserRepository userRepository) {
-//
-//        //todo
-//        return username -> {
-//            User user = userRepository.findByUsername(username).get();
-//            if (user != null) return user;
-//            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
-//        };
-//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
