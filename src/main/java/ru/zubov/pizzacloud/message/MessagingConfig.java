7169f226806c1784d3e62cd1cd1e64c9fc465371
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-11-11 22:59:53 +0300

    Add MappingJackson2MessageConverter

--- src/main/java/ru/zubov/pizzacloud/message/MessagingConfig.java
+++ src/main/java/ru/zubov/pizzacloud/message/MessagingConfig.java
@@ -4,6 +4,11 @@ import jakarta.jms.Destination;
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import ru.zubov.pizzacloud.entity.PizzaOrder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class MessagingConfig {
@@ -11,6 +16,19 @@ public class MessagingConfig {
     @Bean
     public Destination orderQueue() {
         return new ActiveMQQueue("pizzacloud.order.queue");
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("order", PizzaOrder.class);
+        messageConverter.setTypeIdMappings(typeIdMappings);
+
+        messageConverter.setTypeIdPropertyName("_typeId");
+
+        return messageConverter;
     }
 
 }
