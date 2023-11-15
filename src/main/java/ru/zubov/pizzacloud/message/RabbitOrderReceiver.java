c642d737a59eaa755bae05846d333bb1309a4b70
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-11-15 23:18:58 +0300

    Add RabbitOrderReceiver

--- src/main/java/ru/zubov/pizzacloud/message/RabbitOrderReceiver.java
+++ src/main/java/ru/zubov/pizzacloud/message/RabbitOrderReceiver.java
@@ -1,9 +1,10 @@
 package ru.zubov.pizzacloud.message;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import ru.zubov.pizzacloud.entity.PizzaOrder;
 
@@ -13,10 +14,24 @@ public class RabbitOrderReceiver {
     private final RabbitTemplate rabbit;
     private final MessageConverter converter;
 
+    /**
+     * Активный приём сообщений (нужно постоянно вызывать этот метод)
+     * @return PizzaOrder
+     */
     public PizzaOrder receiveOrder() {
-        Message message = rabbit.receive("pizzacloud.order.queue");
-        return message != null
-                ? (PizzaOrder) converter.fromMessage(message)
-                : null;
+        return rabbit.receiveAndConvert("pizzacloud.order.queue",
+                new ParameterizedTypeReference<PizzaOrder>() {});
+    }
+
+    /**
+     * Пассивный приём сообщений (НЕ нужно постоянно вызывать этот метод)
+     *
+     * @param order
+     */
+    @RabbitListener(queues = "tacocloud.order.queue")
+    public void receiveOrder(PizzaOrder order) {
+
+        //Выполнение какого-либо действия
+        //        ui.displayOrder(order);
     }
 }
